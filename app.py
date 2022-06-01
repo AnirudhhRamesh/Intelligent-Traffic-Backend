6fce124ffade30e338e9a62353e995c916541a22
Louis Dumas <louis-dumas@hotmail.fr>
2022-06-02 00:02:16 +0200

    uncoment everything

--- app.py
+++ app.py
@@ -20,12 +20,8 @@ def main():
     print("Starting program...")
 
     #Parse the map
-    #camera = Camera([4, 18, 0,6], [9], 9, 16, goal_ids=[8])
-    #cars = init_cars(myMap, camera.tr)
-    cars = {
-        "Car1 id" : Car("Car1 id", "car1 hardware socket", ""),
-        "Car2 id" : Car("Car2 id", "socket", "")
-    }
+    camera = Camera([4, 18, 0,6], [9], 9, 16, goal_ids=[8])
+    cars = init_cars(myMap, camera.tr)
     myMap = map.Map(map_filename, cars)
     myMap.printMap()
 
@@ -33,27 +29,27 @@ def main():
     #initiate GUI
     gui = GUI.GUI(myMap)
     gui.launchGUI()
-    # april_tag_manager = april_tags()
+    april_tag_manager = april_tags()
 
     #TODO Alexander
     while True:
         gui.update()
-        #camera.update() #get car positions from camera and update global map 
-        #for (id, car) in cars:
-           #car.position(camera.get_pos(car.id))
-           #car.direction(camera.get_dir(car.id))
-           #car.set_goal(camera.get_goal_pos(car.goal_id))
-        #for car in cars:
-        #   car.drive()#find the next point in the path, and set the angle of the car to point there
-        #draw
-        #if cv2.waitKey(1) & 0xFF == ord('q'):
-        #    break
-        #cv2.imshow('camera', camera.frame_in)        
+        camera.update() #get car positions from camera and update global map 
+        for (id, car) in cars:
+           car.position(camera.get_pos(car.id))
+           car.direction(camera.get_dir(car.id))
+           car.set_goal(camera.get_goal_pos(car.goal_id))
+        for car in cars:
+           car.drive()#find the next point in the path, and set the angle of the car to point there
+        draw
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        cv2.imshow('camera', camera.frame_in)        
     for car in cars:
         car.stopDrive()
 
 #Initialize the car connections
-def init_cars(map, tr,cars=[(9, '00:21:09:01:1e:fa')]):
+def init_cars(tr,cars=[(9, '00:21:09:01:1e:fa')]):
     car_list = []
     for car in cars:
         adapter_addr = car[1]
